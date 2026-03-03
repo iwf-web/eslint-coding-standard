a38abfd83addd84b8c2e5dc7c785d9e77c194d49
D3strukt0r <dev@d3strukt0r.dev>
2026-03-03 11:58:15 +0100

    feat: Initial commit
    Release-As: 1.0.0

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export {iwfStandardJs} from './standard-js';
+export {iwfStandardTs} from './standard-ts';
---
+++ src/standard-ts.ts
@@ -0,0 +1,42 @@
+import type {Rules} from '@antfu/eslint-config';
+import antfu from '@antfu/eslint-config';
+import {baseOptions, baseRules} from './standard-js';
+
+const tsRules = {
+  'ts/no-misused-promises': ['off'], // Allow Promises in places like onClick handlers
+  'ts/no-unsafe-assignment': ['off'], // Allow some unsafe ('any') assignments (e.g. form.getFieldValue)
+  'ts/strict-boolean-expressions': ['error', {allowAny: true}], // Allow 'any' in boolean expressions
+  'ts/no-unsafe-argument': ['off'], // Allow using 'any' as function arguments
+  'ts/no-unsafe-member-access': ['off'], // Allow optional chaining on 'any' typed objects (e.g. object._meta)
+  'ts/no-unsafe-return': ['off'], // Allow returning 'any' typed values
+  'ts/no-unsafe-call': ['off'], // Allow calling 'any' typed values
+} satisfies Rules;
+
+// eslint-disable-next-line ts/promise-function-async
+export function iwfStandardTs(
+  options?: Parameters<typeof antfu>[0],
+  ...userConfigs: Parameters<typeof antfu>[1][]
+): ReturnType<typeof antfu> {
+  return antfu(
+    {
+      ...baseOptions,
+      typescript: {
+        tsconfigPath: 'tsconfig.json',
+      },
+      ...options,
+    },
+    {
+      languageOptions: {
+        parserOptions: {
+          projectService: {
+            allowDefaultProject: ['*.js', '*.ts'], // Include TS files in the root project as well
+          },
+        },
+      },
+    },
+    {
+      rules: {...baseRules, ...tsRules},
+    },
+    ...userConfigs,
+  );
+}
